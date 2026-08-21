9a34bcfd54d73d7089fe3240c12a6e8ca4bd0f89
laerciolges17 <laercio.gabriel@escolar.ifrn.edu.br>
2026-08-21 11:42:41 -0300

    21/08/2026

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -6,15 +6,26 @@ public class PlayerControler : MonoBehaviour
     
     [Header("Player Configurations")]
     
+    [SerializeField] private float movementSpeed;
+    
+    private Vector3 direction;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        direction = new Vector3(horizontal, 0f, vertical).normalized;
+        
+        controller.Move(direction * movementSpeed * Time.deltaTime);
         
     }
 }
